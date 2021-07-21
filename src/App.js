d4f66bacf11acf7c9dc41fc19aef952132844194
Ivan Mariic <imariic77@gmail.com>
2021-07-21 12:27:59 +0200

    Simple Image implementation.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, Text, Image, StyleSheet} from 'react-native';
 import {StacksProvider, Stack, Box} from '@mobily/stacks';
+import {Avatar} from 'react-native-elements';
 import {getAllMovies} from './api';
 
 export default App = () => {
@@ -14,11 +15,35 @@ export default App = () => {
     getAllMovies().then(data => setMovies(data));
   }, []);
 
+  console.log(movies[0]);
   return (
     <SafeAreaView>
       <StacksProvider spacing={4}>
+        <Box>
+          <Stack space={4} align="center">
+            <Image
+              style={styles.image}
+              source={{
+                uri: `https://image.tmdb.org/t/p/original/${movies[0]?.poster_path}`,
+              }}
+              resizeMode="stretch"
+            />
+            <Box>
+              <Text>{movies[0]?.title}</Text>
+              <Text>{movies[0]?.vote_average}</Text>
+            </Box>
+          </Stack>
+        </Box>
+
         <Stack space={2}>{constructMoviesList()}</Stack>
       </StacksProvider>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    width: 250,
+    height: 300,
+  },
+});
