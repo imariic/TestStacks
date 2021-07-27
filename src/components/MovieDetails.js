8c166fe39e30676bfdf8906495e0aa6e2ae08ec7
Ivan Mariic <imariic77@gmail.com>
2021-07-27 14:15:38 +0200

    Style improvements.

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,30 +1,56 @@
 import React from 'react';
 import {Image, Text, StyleSheet, ScrollView} from 'react-native';
-import {Rows, Row} from '@mobily/stacks';
-import {generateImageSource} from '../utils';
+import {Rows, Row, useCurrentBreakpoint} from '@mobily/stacks';
+import {generateImageSource, resolveStyle} from '../utils';
 
 const MovieDetails = ({movie}) => {
+  const breakpoint = useCurrentBreakpoint();
   const {title, release_date, poster_path, overview, vote_average} = movie;
+
+  const titleStyle = [
+    styles.title,
+    resolveStyle(styles.titleTablet, styles.titleMobile, breakpoint),
+  ];
+
+  const imageStyle = resolveStyle(
+    styles.imageTablet,
+    styles.imageMobile,
+    breakpoint,
+  );
+
+  const descriptionStyle = resolveStyle(
+    styles.descriptionTablet,
+    styles.descriptionMobile,
+    breakpoint,
+  );
+
+  const releaseDateAndRatingStyle = resolveStyle(
+    styles.releaseAndRatinTablet,
+    styles.releaseAndRatingMobile,
+    breakpoint,
+  );
 
   return (
     <ScrollView>
       <Rows space={4} alignX={['center']} paddingTop={4}>
         <Row width="content" padding={3}>
-          <Text style={styles.title}>{title}</Text>
-          <Text>{overview}</Text>
+          <Text style={titleStyle}>{title}</Text>
+          <Text style={descriptionStyle}>{overview}</Text>
         </Row>
         <Row width="content">
           <Image
             resizeMode="stretch"
-            style={styles.image}
+            style={imageStyle}
             source={generateImageSource(poster_path)}
           />
         </Row>
         <Row width="content">
-          <Text>Release date: {release_date}</Text>
+          <Text style={releaseDateAndRatingStyle}>
+            Release date: {release_date}
+          </Text>
         </Row>
         <Row width="content">
-          <Text>Rating: {vote_average}</Text>
+          <Text style={releaseDateAndRatingStyle}>Rating: {vote_average}</Text>
         </Row>
       </Rows>
     </ScrollView>
@@ -32,15 +58,36 @@ const MovieDetails = ({movie}) => {
 };
 
 const styles = StyleSheet.create({
-  image: {
+  imageMobile: {
     height: 400,
     width: 300,
   },
+  imageTablet: {
+    height: 700,
+    width: 500,
+  },
   title: {
-    fontSize: 20,
     fontWeight: 'bold',
     textTransform: 'uppercase',
     alignSelf: 'center',
+  },
+  titleMobile: {
+    fontSize: 20,
+  },
+  titleTablet: {
+    fontSize: 35,
+  },
+  descriptionMobile: {
+    fontSize: 15,
+  },
+  descriptionTablet: {
+    fontSize: 25,
+  },
+  releaseAndRatingMobile: {
+    fontSize: 20,
+  },
+  releaseAndRatinTablet: {
+    fontSize: 30,
   },
 });
 
