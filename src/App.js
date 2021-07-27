b5171ef0a837ee8ec0b31f28493ba47642b618c3
Ivan Mariic <imariic77@gmail.com>
2021-07-27 15:24:41 +0200

    Typo.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {StacksProvider} from '@mobily/stacks';
-import AppNaviator from './navigator';
+import AppNavigator from './navigator';
 
 export default App = () => {
   return (
     <StacksProvider spacing={2}>
-      <AppNaviator />
+      <AppNavigator />
     </StacksProvider>
   );
 };
